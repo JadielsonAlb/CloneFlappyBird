31a797021c707d4efe27c06dc4be00fb0653e1dc
JadielsonAlb <jadielsonalbapolinario@gmail.com>
2026-05-23 21:18:08 -0300

    canos cima e baixo andando -2 pixel para a esquerda

--- flappybird.js
+++ flappybird.js
@@ -28,6 +28,10 @@ let pipeY = 0;
 let pipeGap = 120; //espaço entre os canos
 let pipeImage;
 let bottomPipeImage;
+
+
+//variável para a gravidade
+let velocityX = -2; 
 //função para carregar o jogo
 window.onload = function() {
     board = document.getElementById("board");
@@ -57,19 +61,40 @@ setInterval(placePipes, 1500); //chamar a função placePipes a cada 1.5 segundo
 function update() {
     requestAnimationFrame(update);
     context.clearRect(0, 0, board.width, board.height);
+   
+  context.drawImage(birdImage, bird.x, bird.y, bird.width, bird.height);
 
-    ///desenhar o pássaro novamente para atualizar a posição
-    context.drawImage(birdImage, bird.x, bird.y, bird.width, bird.height);
+   // canos
+    for(let i = 0; i < pipeArray.length; i++) {
+        let pipe = pipeArray[i];
+        pipe.x += velocityX; //mover os canos para a esquerda
+        context.drawImage(pipe.img, pipe.x, pipe.y, pipe.width, pipe.height);
 }
-
+}
+///função para colocar os canos
 function placePipes() {
+    
+    let RandomPipeY = pipeY - pipeHeight/4 - Math.random()*(pipeHeight/2); //gerar uma posição y aleatória para o cano superior
+    let openingSpace = board.height/4; //espaço entre os canos
     let topPipe ={
     img: topPipeImage,
     x: pipeX,
-    y: pipeY,
+    y: RandomPipeY,
     width: pipeWidth,
     height: pipeHeight,
     passed: false
 }
     pipeArray.push(topPipe);
-}
+
+    let bottonPipe  ={
+        img: bottomPipeImage,
+        x : pipeX,
+        y: RandomPipeY + pipeHeight + openingSpace,
+        width: pipeWidth,
+        height: pipeHeight,
+        passed: false
+    }
+    pipeArray.push(bottonPipe);
+
+    }
+
